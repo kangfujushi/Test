c47a4b75634672ced5b5a43e3a5899fe305c9c5e
卫宫巨侠欧尼酱 <zhaoningdeemail@163.com>
2018-12-13 01:32:31 +0800

    创建项目

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 赵宁 on 2018/12/13.
+//  Copyright © 2018 赵宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 赵宁 on 2018/12/13.
+//  Copyright © 2018 赵宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
